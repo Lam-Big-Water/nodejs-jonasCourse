da8b30360a508a7cc7faf0e28c11deaba0e38397
Sam Lam <xlin67948gmail.com>
2024-07-13 19:07:51 +0800

    009-Non-Blocking

---
+++ 1-node-farm/fsModule/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./test.txt', 'utf-8');
+
+console.log(textIn);
+
+
+const textOut = `This is what we know about: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./test.txt', textOut);
+console.log('File written!');
